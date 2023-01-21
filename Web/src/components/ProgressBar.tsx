4937b59aac3e72e7ab3a0e9b42270d0a532ba8af
Thiago Suzuki <thiagoikenaga@gmail.com>
2023-01-21 01:50:17 -0300

    Aula 5 - Finalizada

--- Web/src/components/ProgressBar.tsx
+++ Web/src/components/ProgressBar.tsx
@@ -1,16 +1,19 @@
+import * as Progress from '@radix-ui/react-progress';
 interface ProgressBarProps {
     progress: number;
 }
 
 export function ProgressBar(props: ProgressBarProps) {
     return (
-        <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+        <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
             <div
                 role="progressbar"
                 aria-label="Progresso de hábitos completados nesse dia"
                 aria-valuenow={props.progress}
-                className="h-3 rounded-xl bg-violet-600"
-                style={{ width: `${props.progress}%` }}
+                className="h-3 rounded-xl bg-violet-600 transition-all"
+                style={{
+                    width: `${props.progress}%`
+                }}
             />
         </div>
     )
